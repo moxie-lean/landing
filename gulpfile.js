ed8b795a8f6fcbee444c99ec694f5dba787169bb
Orçun Ilbeyli <nucrotion@gmail.com>
2015-11-19 02:21:39 +0200

    Folder removal and Optimizations
    Removed bower_components and made some optimizations on files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,98 @@
 var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
+  plumber = require('gulp-plumber'),
+  rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache');
+  cache = require('gulp-cache');
 var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
+
+var project = '.';
+var source = project + '/assets/';
+var bower = project + '/bower_components/';
 
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
-       baseDir: "./"
+      baseDir: "./"
     }
   });
 });
 
-gulp.task('bs-reload', function () {
+gulp.task('bs-reload', function() {
   browserSync.reload();
 });
 
-gulp.task('images', function(){
+gulp.task('images', function() {
   gulp.src('src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(cache(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
     .pipe(gulp.dest('dist/images/'));
 });
+gulp.task('js:combine', function() {
+  var scripts = [
+    bower + 'jquery/dist/jquery.min.js',
+    bower + 'bxslider-4/dist/jquery.bxslider.min.js'
 
-gulp.task('styles', function(){
+  ];
+  return gulp.src(scripts)
+    .pipe(sourcemaps.init())
+    .pipe(concat('production.js'))
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('styles', function() {
   gulp.src(['src/styles/**/*.scss'])
     .pipe(plumber({
-      errorHandler: function (error) {
+      errorHandler: function(error) {
         console.log(error.message);
         this.emit('end');
-    }}))
+      }
+    }))
     .pipe(sass())
     .pipe(autoprefixer('last 2 versions'))
     .pipe(gulp.dest('dist/styles/'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(minifycss())
     .pipe(gulp.dest('dist/styles/'))
-    .pipe(browserSync.reload({stream:true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('scripts', function(){
+gulp.task('scripts', function() {
   return gulp.src('src/scripts/**/*.js')
     .pipe(plumber({
-      errorHandler: function (error) {
+      errorHandler: function(error) {
         console.log(error.message);
         this.emit('end');
-    }}))
+      }
+    }))
     .pipe(concat('main.js'))
     .pipe(gulp.dest('dist/scripts/'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(uglify())
     .pipe(gulp.dest('dist/scripts/'))
-    .pipe(browserSync.reload({stream:true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('default', ['browser-sync'], function(){
+gulp.task('default', ['browser-sync'], function() {
   gulp.watch("src/styles/**/*.scss", ['styles']);
   gulp.watch("src/scripts/**/*.js", ['scripts']);
   gulp.watch("*.html", ['bs-reload']);
+  gulp
 });
